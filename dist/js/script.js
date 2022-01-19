2f75d10e441b901db3cbd6600c674eacba3c4755
Michail <62438107+Frai-i14@users.noreply.github.com>
2022-01-19 23:07:15 +0300

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+
+    $('.header__wrap_block_menu').on( "click", function(event) {
+        if(event.target.tagName == 'LI'){
+            $('.hamburger-menu').click();
+            let a = 0;
+            $('.header__wrap_menu_item').each(function(index, el){
+                el.classList.remove('menu_item-active');
+                if(el == event.target) a = index;
+            });
+
+            $('.main__wrap_content_block').each(function(index, el){
+                el.classList.remove('block-active');
+            });
+
+            $('.main__wrap_content_block')[a].classList.add('block-active');
+            event.target.classList.add('menu_item-active');
+        }
+        else{
+            return;
+        }
+      });
+
+    
+    $('input[type=file]').on("change", function(){
+        let str = this.files[0].name.slice(0,3) + '..' + this.files[0].name.slice(-4);
+        let el = document.createElement('div');
+        el.classList.add('file_name');
+        el.innerHTML = `<span></span><small>${str}</small>`;
+        document.querySelector('.input_wrapper').appendChild(el);
+        document.querySelector('.input_wrapper').style.width = '300px';
+        document.querySelector('.label-file').innerHTML = '<img src="./icons/Plus.svg" alt="icon">';
+    })
+
+    $('.hamburger-menu').on('click', function() {
+        $('.bar').toggleClass('animate');
+        $('.header__wrap_block').toggleClass('block');
+    });
+
+    function toggleBlockWork(elem){
+        elem.parentNode.parentNode.classList.remove('work_active');
+        elem.parentNode.parentNode.nextElementSibling.classList.add('work_active');
+        if (window.screen.width <= 992){
+            elem.parentNode.parentNode.nextElementSibling.scrollIntoView({block: "center", inline: "center", behavior: "smooth"});
+        }
+        $('.work_active .btn').on('click', function (event){
+            event.preventDefault();
+            toggleBlockWork(this);
+        });
+    };
+
+    $('.work_active .btn').on('click', function (event){
+        event.preventDefault();
+        toggleBlockWork(this);
+    });
+
+
+    $('.work__wrap_block_item').on('click', function (){
+        $('.work__wrap_block').each(function(index, el){
+            el.classList.remove('work_active');
+        });
+        this.parentNode.classList.add('work_active');
+        if (window.screen.width <= 992){
+            this.parentNode.scrollIntoView({block: "center", inline: "center", behavior: "smooth"});
+        }
+
+    })
+
+    new WOW().init();
+});
+
+
+
+
